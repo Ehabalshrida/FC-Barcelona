33fc712e91050b2749e09ad5fbbc619146a84c77
Ehabalshrida <>
2021-06-30 13:49:19 +0000

    added html and js files

---
+++ index.js
@@ -0,0 +1,10 @@
+ var barca = confirm("Are you Barca Fans")
+    if (barca == true){
+      console.log(barca)
+      alert("You are welcome")
+      var fan = prompt("insert your name")
+      document.write("<h2>"+fan+"</h2>")
+    } 
+    else{
+      alert("Nice To meet You")
+    } 
